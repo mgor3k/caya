6451e149914bfcb5c7ed0b2cb73565bfb4d0fb3c
Maciej Górecki <maciekgorecki@icloud.com>
2021-10-09 17:41:03 +0200

    Project init

---
+++ Caya/CayaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CayaApp.swift
+//  Caya
+//
+//  Created by Maciej Górecki on 09/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct CayaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
